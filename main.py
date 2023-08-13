f794dc5d9e0b5b6f86b32f945f221a1a806d8588
jkf87 <jjoongoo@gmail.com>
2023-08-14 02:30:12 +0900

    bugfix.py
    메모리 관리, try-error, 일일 1회만 체크

--- main.py
+++ main.py
@@ -1,73 +1,96 @@
-
 import cv2
 import numpy as np
 import face_recognition
 import os
-import gc
-import time
 from datetime import datetime
-from datetime import date
 import pytz
 import csv
 
+# 이미지 인코딩 함수 (Function to encode images)
 def identifyEncodings(images):
     encodeList = []
     for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         try:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             encode = face_recognition.face_encodings(img)[0]
             encodeList.append(encode)
         except Exception as e:
-            print(f"Error encoding image: {e}")
+            print(f"이미지 인코딩 오류 (Error encoding image): {e}")
     return encodeList
 
+# 출석 기록 함수 (Function to mark attendance)
 def markAttendance(name):
-    date = datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%y_%m_%d||%H:%M")
+    # 파일 이름에는 날짜만 포함 (Only include date in filename)
+    date = datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%y_%m_%d")
+    file_path = f'Attendance_Entry/Attendance_{date}.csv'
+    
+    # Check if file exists
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+
     try:
-        with open(f'Attendance_Entry/Attendance_{date}.csv', 'r+') as f:
+        with open(file_path, 'r+') as f:
             myDataList = f.readlines()
-            nameList = [line.split(',')[0] for line in myDataList]
+            nameList = []
+            for line in myDataList:
+                entry = line.split(',')
+                nameList.append(entry[0])
+            
+            # 이미 출석한 경우 추가 출석을 하지 않음 (If already attended, do not add again)
             if name not in nameList:
                 now = datetime.now()
                 dtString = now.strftime('%H:%M:%S')
                 date_i = now.strftime('%Y-%m-%d')
-                f.writelines(f'
-{name},{dtString},{date_i}')
+                f.writelines(f'\n{name},{dtString},{date_i}')
     except Exception as e:
-        print(f"Error marking attendance: {e}")
+        print(f"출석 기록 오류 (Error marking attendance): {e}")
 
-date = datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%y_%m_%d||%H:%M")
-header = ("S.NO","Time","Date")
+
+# 'Attendance_Entry' 디렉터리가 있는지 확인 후 생성 (Check and create 'Attendance_Entry' directory)
+if not os.path.exists('Attendance_Entry'):
+    os.makedirs('Attendance_Entry')
+
+# 파일 이름에는 날짜만 포함 (Only include date in filename)
+date = datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%y_%m_%d")
+header = ("S.NO", "Time", "Date")
 
 try:
-    with open(f"Attendance_Entry/Attendance_{date}.csv","w") as file:
-        writer = csv.writer(file)
-        writer.writerow(header)
+    if not os.path.exists(f"Attendance_Entry/Attendance_{date}.csv"):
+        with open(f"Attendance_Entry/Attendance_{date}.csv", "w") as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
 except Exception as e:
-    print(f"Error creating attendance file: {e}")
+    print(f"초기 CSV 생성 오류 (Error creating initial CSV): {e}")
 
-path = 'Attendance_data'
-images = []
-classNames = []
-myList = os.listdir(path)
-for cl in myList:
-    try:
+try:
+    path = 'Attendance_data'
+    images = []
+    classNames = []
+    myList = os.listdir(path)
+    for cl in myList:
         curImg = cv2.imread(f'{path}/{cl}')
         images.append(curImg)
         classNames.append(os.path.splitext(cl)[0])
-    except Exception as e:
-        print(f"Error reading image: {e}")
+    encodeListKnown = identifyEncodings(images)
+    del images  # 인코딩 후 메모리 해제 (Free up memory after encoding)
+    print('인코딩 완료 (Encoding Complete)')
+except Exception as e:
+    print(f"전처리 중 오류 (Error during preprocessing): {e}")
 
-encodeListKnown = identifyEncodings(images)
-gc.collect()
-print('Encoding Complete')
-
-cap = cv2.VideoCapture('/dev/video0')
-start_time = time.time()
-
-while True:
-    try:
+try:
+    cap = cv2.VideoCapture(1)
+    if not cap.isOpened():
+        print("비디오 스트림을 열 수 없음 (Error: Could not open video stream).")
+        exit()
+    
+    while True:
         success, img = cap.read()
+        if not success or img is None:
+            print("비디오 스트림에서 프레임을 읽는 중 오류 (Error: Failed to read frame from video stream).")
+            continue
+
         imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
         imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
@@ -88,16 +111,11 @@ while True:
                 cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
                 markAttendance(name)
 
-        cv2.imshow('Attendance System', img)
+        cv2.imshow('출석 시스템 (Attendance System)', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-        if time.time() - start_time > 3600:
-            encodeListKnown = []
-            start_time = time.time()
-
-    except Exception as e:
-        print(f"Error in main loop: {e}")
-
-cap.release()
-cv2.destroyAllWindows()
+except Exception as e:
+    print(f"얼굴 인식 중 오류 (Error during face recognition): {e}")
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
